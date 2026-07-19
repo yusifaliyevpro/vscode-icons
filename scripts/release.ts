0d860b2a76c660f4307211adf5cbd8aee0f28295
Yusif Aliyev <yusifaliyevpro@gmail.com>
2026-07-19 16:41:48 +0400

    refactor: Simplify JSON parsing for package version in release script

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,9 +1,7 @@
 import { execFileSync } from "node:child_process";
 import { readFileSync } from "node:fs";
 
-const { version } = JSON.parse(
-  readFileSync(new URL("../package.json", import.meta.url), "utf8"),
-);
+const { version } = JSON.parse(readFileSync(new URL("../package.json", import.meta.url), "utf8"));
 
 const git = (args: string[]) => execFileSync("git", args, { stdio: "inherit" });
 
